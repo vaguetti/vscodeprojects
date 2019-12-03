5a98c904f8dadadcafbf1fae8f40ec2dbdd5f423
unknown <leandro.vaguetti@ifb.edu.br>
2019-12-03 14:36:47 -0600

    atualização da API Rest (verbos&params)

--- ReactBootstrap/backend/src/apirestbasic.js
+++ ReactBootstrap/backend/src/apirestbasic.js
@@ -1,12 +1,35 @@
 const express = require('express');
+// GET, PUT, DELETE, POST
 
 const app = express();
-
 
 app.get('/', function(req, res) {
 
      res.json({ nome : "João", idade: 30 , telefone : "(61) 99999-9999"});
   
 });
+// query params http://localhost:4000/alunos?idade=20&nome=Pedro
+app.get('/alunos', function(req, res) {
 
+     res.json({ idade : req.query.idade});
+  
+});
+// route params http://localhost:4000/alunos/3
+app.delete('/alunos/:id', function(req, res) {
+
+     res.json({ id : req.params.id});
+  
+});
+app.put('/alunos/:nome', function(req, res) {
+
+     res.json({ nome : req.params.nome});
+  
+});
+// body params http://localhost:4000/alunos
+app.use(express.json())
+app.post('/alunos', function(req, res) {
+
+     res.json(req.body);
+
+});
 app.listen(4000);
