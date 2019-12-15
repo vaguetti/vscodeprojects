2881750b4345d772f63c104a2ab125bb9c2727b7
unknown <leandro.vaguetti@ifb.edu.br>
2019-12-15 15:23:49 -0300

    atualização com build

---
+++ ReactBootstrap/frontend/build/precache-manifest.b374854960797a78df0da9ce996e7af7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95a3167a95b733ec78964742441c43fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a64c6e3655c3ff02bdb9",
+    "url": "/static/css/2.83ace7dd.chunk.css"
+  },
+  {
+    "revision": "b3f346b0cb896d5a722b",
+    "url": "/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "a64c6e3655c3ff02bdb9",
+    "url": "/static/js/2.35274bfb.chunk.js"
+  },
+  {
+    "revision": "b3f346b0cb896d5a722b",
+    "url": "/static/js/main.6db3a257.chunk.js"
+  },
+  {
+    "revision": "aa5dd861c3df341bfb4f",
+    "url": "/static/js/runtime-main.0fda84ed.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
